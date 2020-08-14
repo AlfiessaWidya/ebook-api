58b5cbfca878b4c376e295335576d6a1f143538a
Alfiessa Widya Wirawan <alfiessam.widya@gmail.com>
2020-08-14 22:55:56 +0700

    Upload migration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return "NIS : 3103118010
+        Nama : Alfiessa Widya Wirawan
+        Gender : Laki-Laki
+        Phone : 089647205953
+        Kelas : XII RPL 1";
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Alfiessa Widya
+                    Alfiessa Widya W
                 </div>
 
                 <div class="links">
